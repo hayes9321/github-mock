37393c040f96175fed683032a4dec012c906b35e
Travis <travis@travismhayes.com>
2021-12-09 17:54:39 -0800

    Update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '14': '3.5rem',
+        '22': '5.5rem',
+        '72': '18rem',
+        '22': '50rem'
+      }
+    },
   },
   variants: {
     extend: {},
