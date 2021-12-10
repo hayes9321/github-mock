63cd225841dcf9282895f4b7210806ee344ce34b
Travis <travis@travismhayes.com>
2021-12-10 05:55:59 -0800

    add repo-status component

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,11 @@
 import Nav from './nav'
+import RepoStats from './repo-stats'
 
 const Header = () => {
   return (
-    <div>
+    <div class="text-gray-900 text-sm">
       <Nav/>
+      <RepoStats/>
     </div>
   )
 }
