95a76d9f56b442954c8dc998fb6a098e8b97b05d
Travis <travis@travismhayes.com>
2021-12-12 10:30:08 -0800

    add file explorer component

---
+++ src/components/file-explorer.js
@@ -0,0 +1,11 @@
+import BranchNav from './branch-nav'
+
+const FileExplorer = () => {
+  return (
+    <div className="w-full lg:w-3/4 lg:mr-8">
+      <BranchNav/>
+    </div>
+  )
+}
+
+export default FileExplorer;
