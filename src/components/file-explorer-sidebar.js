02c82d7e8fa57acff339de6cca99ffe1c3710779
Travis <travis@travismhayes.com>
2021-12-12 10:29:13 -0800

    add explorer sidebar component

---
+++ src/components/file-explorer-sidebar.js
@@ -0,0 +1,17 @@
+
+const FileExplorerSideBar = () => {
+  return (
+    <div class="sidebar w-full lg:w-1/4">
+      sidebar
+      {/* about */}
+      {/* releases */}
+
+      {/* used-by */}
+
+      {/* contributors */}
+      {/* languages */}
+    </div>
+  )
+}
+
+export default FileExplorerSideBar;
